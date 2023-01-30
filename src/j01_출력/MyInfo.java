d198f17c7a45e362a066214499f84a3a56cf7f91
Jonghak Kim <whdgkr9070@naver.com>
2023-01-30 23:52:48 +0900

    2023.01.30

---
+++ src/j01_출력/MyInfo.java
@@ -0,0 +1,9 @@
+package j01_출력;
+
+public class MyInfo {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ src/j01_출력/PrintTest.java
@@ -0,0 +1,26 @@
+package j01_출력;
+
+public class PrintTest {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");
+		System.out.print("Hello, java");
+		System.out.print("김종학");
+		System.out.println("----------------");
+		
+		System.out.print("Hello, Java\n");
+		System.out.print("김종학");
+		
+		
+		// println은 줄바꿈해라는 뜻
+		/*
+		 *  장문주석 
+		 */
+		/*
+		 * 장문주석
+		 * 장문주석2
+		 * */
+
+	}
+
+}
